52fc48df26d6c85cc89e96e8cde67aea2a312043
Yunli Tang <tangy@us.ibm.com>
2017-09-06 15:17:06 -0400

    Change python script to make more general

--- python/data/ivp-load.py
+++ python/data/ivp-load.py
@@ -1,7 +1,7 @@
 import dsdbc
 import pandas as pd
 
-df = pd.read_csv('data/german.data', delimiter=' ', 
+df = pd.read_csv('ivp-german-data.csv', delimiter=',', 
                             names = ["checkingAccount",
                                      "duration",
                                      "creditHistory",
@@ -24,11 +24,19 @@ df = pd.read_csv('data/german.data', delimiter=' ',
                                      "foreign",
                                      "risk"])
 
-conn = dsdbc.connect(ssid="AZKS")
+#Replace ssid with the subsystem ID of the local data service 
+#server. If this is not specified, the name will be selected based on 
+#the server group, 'sgrp', or if not provided, the first subsystem with 
+#a Data Service will be used. For more information please run help(dsdbc)
+ssid = "<SUBSYSTEM_ID_OF_LOCAL_DATA_SERVICE_SERVER>"
+
+#Replace ps_dataset with the name of the empty physical sequential dataset
+ps_dataset = "<NAME_OF_PHYSICAL_SEQUENTIAL_DATASET>"
+conn = dsdbc.connect(ssid=ssid)
 cursor = conn.cursor()
 for index, row in df.iterrows():
     cursor.execute(
-      "insert into CREDIT_DATA values({0},'{1}',{2},'{3}','{4}',{5},'{6}','{7}',{8},'{9}','{10}',{11},'{12}',{13},'{14}','{15}',{16},'{17}',{18},'{19}','{20}',{21})".format(
+      "insert into " + ps_dataset + " values({0},'{1}',{2},'{3}','{4}',{5},'{6}','{7}',{8},'{9}','{10}',{11},'{12}',{13},'{14}','{15}',{16},'{17}',{18},'{19}','{20}',{21})".format(
       index,
       row[0],
       row[1],
@@ -51,4 +59,3 @@ for index, row in df.iterrows():
       row[18],
       row[19],
       row[20]))
-
